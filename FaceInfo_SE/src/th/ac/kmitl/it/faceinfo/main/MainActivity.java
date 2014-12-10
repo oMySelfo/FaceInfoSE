8d3368d4c66184c727f8ae60a19b4863af0cebdc
Nattawit <msn_coach@hotmail.com>
2014-12-10 12:13:53 +0700

    เปลี่ยน Theme
    error อะไรไม่รู้เลยเปลี่ยน

--- FaceInfo_SE/src/th/ac/kmitl/it/faceinfo/main/MainActivity.java
+++ FaceInfo_SE/src/th/ac/kmitl/it/faceinfo/main/MainActivity.java
@@ -2,8 +2,6 @@ package th.ac.kmitl.it.faceinfo.main;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
