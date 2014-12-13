c275c8604f8fca20bc00950c169a0168e2bf3fa5
Kkla <sirawit14@gmail.com>
2014-12-13 14:00:03 +0700

    :D

--- src/th/ac/kmitl/it/faceinfo/main/MainActivity.java
+++ src/th/ac/kmitl/it/faceinfo/main/MainActivity.java
@@ -2,12 +2,21 @@ package th.ac.kmitl.it.faceinfo.main;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends Activity {
+	
+	private String[] navMenuTitles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items);
+        
     }
+    
+    
+    
+    
 }
