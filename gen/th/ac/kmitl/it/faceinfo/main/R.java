ea124b537e21903704804226cb3e7cdb6e9e3bb5
Kkla <sirawit14@gmail.com>
2014-12-13 13:07:21 +0700

    min 14

--- gen/th/ac/kmitl/it/faceinfo/main/R.java
+++ gen/th/ac/kmitl/it/faceinfo/main/R.java
@@ -33,10 +33,13 @@ public final class R {
     }
     public static final class id {
         public static final int frame_container=0x7f080000;
+        public static final int icon=0x7f080002;
         public static final int slidermenu=0x7f080001;
+        public static final int title=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int item_in_slidermenu=0x7f030001;
     }
     public static final class string {
         public static final int app_id=0x7f060001;
