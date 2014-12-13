79fe2e5819ddb832d36b0393d94eb360cdcdc3c0
Kkla <sirawit14@gmail.com>
2014-12-13 15:41:54 +0700

    Add slidermenu

--- src/th/ac/kmitl/it/faceinfo/main/MainActivity.java
+++ src/th/ac/kmitl/it/faceinfo/main/MainActivity.java
@@ -1,28 +1,112 @@
 package th.ac.kmitl.it.faceinfo.main;
 
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 public class MainActivity extends Activity {
-	
-	private String[] navMenuTitles;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Data.getData().setDatabaseManager(this);
-        navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items);
-        
-        
-        
-        
-        
-        
-    }
-    
-    
-    
-    
+	private String[] navMenuTitles;
+	private DrawerLayout mDrawerLayout;
+	private ActionBarDrawerToggle mDrawerToggle;
+	private ListView mDrawerList;
+	private CharSequence mTitle;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Data.getData().setDatabaseManager(this);
+		mTitle = getTitle();
+		navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items);
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.main_frame);
+		mDrawerList = (ListView) findViewById(R.id.slidermenu);
+		mDrawerList.setOnItemClickListener(new SlideMenuClickListener());
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+		SetMenuSliding();
+
+	}
+
+	private class SlideMenuClickListener implements
+			ListView.OnItemClickListener {
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+			displayView(position);
+		}
+	}
+	
+	public void displayView(int position) {
+		
+	}
+	
+	
+	private void SetMenuSliding() {
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+				R.drawable.ic_drawer, R.string.app_name, R.string.app_name) {
+			public void onDrawerClosed(View view) {
+				getActionBar().setTitle(mTitle);
+				invalidateOptionsMenu();
+			}
+
+			public void onDrawerOpened(View drawerView) {
+				getActionBar().setTitle(mTitle);
+				invalidateOptionsMenu();
+			}
+		};
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+		
+		String[][] dataSliding = new String[][] {
+				{ R.drawable.ic_contacts + "", "All Contacts" },
+				{ R.drawable.ic_aboutus + "", "About Us" },
+				{ R.drawable.bt_plus + "", "Add Contacts" },
+				{ R.drawable.ic_group + "", "Group" }, 
+				{R.drawable.bt_plus+"","Test"}};
+		String[] from = { "icon", "title" };
+		int[] to = { R.id.icon, R.id.title };
+		List<HashMap<String, String>> listSliding = new ArrayList<HashMap<String, String>>();
+		for (int i = 0; i < dataSliding.length; i++) {
+			HashMap<String, String> hm = new HashMap<String, String>();
+			hm.put("icon", dataSliding[i][0]);
+			hm.put("title", dataSliding[i][1]);
+			listSliding.add(hm);
+		}
+		SimpleAdapter adapterSliding = new SimpleAdapter(this, listSliding,
+				R.layout.item_in_slidermenu, from, to);
+
+		mDrawerList.setAdapter(adapterSliding);
+	}
+	
+	
+	
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+
+		// Handle action bar actions click
+		switch (item.getItemId()) {
+//		 case R.id.action_settings:
+//			 return true
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+
 }
