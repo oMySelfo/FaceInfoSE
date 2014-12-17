be91e81b63261f6dd828a6970842a685515aa803
jays1234 <02584561397a>
2014-12-18 04:51:52 +0700

    change resize

--- src/th/ac/kmitl/it/faceinfo/main/MainActivity.java
+++ src/th/ac/kmitl/it/faceinfo/main/MainActivity.java
@@ -54,6 +54,7 @@ public class MainActivity extends Activity {
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		displayView(9);
 		getActionBar().setHomeButtonEnabled(true);
+		
 		SetMenuSliding();
 
 	}
