0aa92ab9f66f9477b4c1d24a2db96f9b3d5375fb
Kkla <sirawit14@gmail.com>
2014-12-17 16:32:53 +0700

    Find Button

--- src/th/ac/kmitl/it/faceinfo/allfragment/AllContract.java
+++ src/th/ac/kmitl/it/faceinfo/allfragment/AllContract.java
@@ -1,22 +1,42 @@
 package th.ac.kmitl.it.faceinfo.allfragment;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
+import org.json.JSONException;
 
 import th.ac.kmitl.it.faceinfo.adapter.AllContactAdapter;
 
 import th.ac.kmitl.it.faceinfo.database.Contact;
 import th.ac.kmitl.it.faceinfo.database.DatabaseManager;
+import th.ac.kmitl.it.faceinfo.database.Photo;
 import th.ac.kmitl.it.faceinfo.main.Data;
+import th.ac.kmitl.it.faceinfo.main.MainActivity;
 import th.ac.kmitl.it.faceinfo.main.R;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Images.Media;
 import android.support.v4.widget.DrawerLayout;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.AdapterView;
+import android.widget.ImageButton;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
@@ -26,11 +46,20 @@ public class AllContract extends Fragment {
 	private List<Contact> listContact;
 	private Data data;
 	private DatabaseManager dbm;
+	private View rootView;
+	private Bitmap bitmap;
+	private Uri uri;
+	private String path;
+	private int REQUEST_GALLERY = 1;
+	private int REQUEST_CAMERA = 2;
+	private MainActivity ma;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		View rootView = inflater.inflate(R.layout.list_allcontract, container,false);
+		rootView = inflater.inflate(R.layout.list_allcontract, container,false);
+		ma = Data.getData().getMainActivity();
 		
 		data = Data.getData();
 		dbm = data.getDmb();
@@ -47,13 +76,87 @@ public class AllContract extends Fragment {
 				// TODO Auto-generated method stub
 				data.setTempKey(listContact.get(position).getCon_id());
 				data.getMainActivity().displayView(8);
-				
-
-				
 			}
 		});
+		showConTextMenu();
+		
+		
 		return rootView;
 	}
+	
+	private void showConTextMenu(){
+		ImageButton findbtt = (ImageButton) rootView.findViewById(R.id.findbtt);
+		registerForContextMenu(findbtt);
+		findbtt.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				 ((ImageButton)v).showContextMenu();
+			}
+		});
+		
+	}
+	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		menu.add(0, v.getId(), 0, "TakePicture");
+		menu.add(0, v.getId(), 0, "Gallery");
+	}
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		if (item.getTitle() == "TakePicture") {
+			System.out.println("TakePicture");
+			Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+			String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+					.format(new Date());
+			String imageFileName = "IMG_" + timeStamp + ".jpg";
+			File f = new File(Environment.getExternalStorageDirectory(),
+					"DCIM/Camera/" + imageFileName);
+			uri = Uri.fromFile(f);
+			path = uri.getPath();
+			it.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+			startActivityForResult(Intent.createChooser(it, "Take a picture"),
+					REQUEST_CAMERA);
+
+		} else if (item.getTitle() == "Gallery") {
+			System.out.println("Gallery");
+			Intent itGallery = new Intent(Intent.ACTION_GET_CONTENT);
+			itGallery.setType("image/*");
+			startActivityForResult(
+					Intent.createChooser(itGallery, "Select Picture"),
+					REQUEST_GALLERY);
+		} else {
+			return false;
+		}
+		return true;
+
+	}
+	
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		try {
+			if (requestCode == REQUEST_CAMERA && resultCode == ma.RESULT_OK) {
+				ma.getContentResolver().notifyChange(uri, null);
+				ContentResolver cr = ma.getContentResolver();
+				bitmap = Media.getBitmap(cr, uri);
+
+			} else if (requestCode == REQUEST_GALLERY
+					&& resultCode == ma.RESULT_OK) {
+				Uri uri = data.getData();
+				bitmap = Media.getBitmap(ma.getContentResolver(), uri);
+
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(bitmap);
+
+	}
+	
+	
 
 
 	
